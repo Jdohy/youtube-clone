c41d83b3b09b38bc17c3a376d07fa1a4356a351d
Jdohy <doldol92877@gmail.com>
2022-07-24 16:25:41 +0900

    commit

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("💻 Connected to DB ");
+const handleError = (error) => console.log("🚫 DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
