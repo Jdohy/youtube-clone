fb03b23c6fdb9f14f6923e44aecf2a0d3e1e74ee
Jdohy <doldol92877@gmail.com>
2022-10-07 17:18:40 +0900

    browser-fix

--- src/server.js
+++ src/server.js
@@ -17,9 +17,9 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
