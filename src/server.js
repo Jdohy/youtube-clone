6db3e6f05ba192f613553898e1dc260544bf12c2
Jdohy <doldol92877@gmail.com>
2022-10-07 17:24:55 +0900

    browser-fix

--- src/server.js
+++ src/server.js
@@ -22,15 +22,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
